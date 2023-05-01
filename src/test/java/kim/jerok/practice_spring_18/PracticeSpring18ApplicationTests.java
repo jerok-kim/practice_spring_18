b32898772cd5e7c88b91f97d6c9b2da8f7cf8dd1
jerok <jerok.kim@gmail.com>
2023-05-01 17:02:52 +0900

    practice spring

---
+++ src/test/java/kim/jerok/practice_spring_18/PracticeSpring18ApplicationTests.java
@@ -0,0 +1,13 @@
+package kim.jerok.practice_spring_18;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeSpring18ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
